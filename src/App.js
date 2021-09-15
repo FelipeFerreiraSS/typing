55c36441f1f40ddeae72dda4480ba73eef718ab3
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-09-15 17:07:49 -0300

    KeyDown

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App(){
+    const [typeKeys, setTypeKeys] = useState([])
+    const handleKeyDown = (e) => {
+        e.preventDefault()  
+        const { key } = e
+
+        setTypeKeys((prevTypeKeys) => {
+            return [...prevTypeKeys, key].slice(-30)
+        })
+
+        console.log(key)
+    }
+
     return(
-        <div className="container">
+        <div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
             <div className="valid-keys">
                 <span className="matcherd">Feli</span> 
                 <span className="remainder">pe</span>
             </div>
-            <div className="type-keys">sssdsadgdgsg</div>
+            <div className="type-keys">{typeKeys ? typeKeys.join(' ') : null}</div>
             <div className="completed-words">
                 <ol>
                     <li>casa</li>
